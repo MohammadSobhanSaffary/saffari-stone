5b916683f02e6eca3e494f7c8e9a17c88633fad3
Sobhan <sobhandanger81@gmail.com>
2022-12-16 19:27:53 +0330

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import i18n from "i18next";
+import { initReactI18next, useTranslation } from "react-i18next";
+import { messageEn } from "./constants/messageEn";
+import { messageFa } from "./constants/messageFa";
+import i18next from "i18next";
+
+const resources = {
+  en: {
+    translation: messageEn,
+  },
+  fa: {
+    translation: messageFa,
+  },
+};
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: "en",
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  });
 
 function App() {
+  const { t } = useTranslation();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App  text-3xl font-bold underline">{t("SaffariStone")}</div>
   );
 }
 
